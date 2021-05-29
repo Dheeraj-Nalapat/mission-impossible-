db5da8a8a6266d3d40a4cab4263c332fe720a257
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-05-29 20:47:15 +0530

    Create Madlib.py

---
+++ pythonprojects/Madlib.py
@@ -0,0 +1,13 @@
+adj1= input("Adjective 1:")
+adj2= input("adjective 2:")
+noun1 = input("Noun 1: ")
+verb1 = input("verb1: ")
+noun2= input("noun2: ")
+adj3 = input("adjective 3 : ")
+verb2 = input("noun 2:")
+noun3 = input("noun3 :")
+
+madlib= f"coding is {adj1} hence {adj2} . so {noun1} are going to start coding , rather than {verb1} ." \
+        f"so {noun2} get started to a {adj3} {verb2} {noun3} ."
+
+print(madlib)
