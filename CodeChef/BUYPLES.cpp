38384814cf4714bc29aa8baae287302600ce63e9
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-05-29 21:06:57 +0530

    Create BUYPLES.cpp

---
+++ CodeChef/BUYPLES.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int N;
+   cin>>N;
+   if(N%5==0 && N%11 == 0){
+       cout<<"BOTH"<<endl;
+      
+   }
+   else if(N%5 ==0 || N%11 == 0) {
+     cout<<"ONE"<<endl;
+   }
+   else{
+       cout<<"NONE"<<endl;
+       
+   }
+}
