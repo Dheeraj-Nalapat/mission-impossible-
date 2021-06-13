dd1d84ef0381a273c3ef0b425e3324b6bc385f59
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-06-13 23:42:07 +0530

    Create Lab7.c

---
+++ S2LAB/Lab7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main(){
+    int n,temp=0;
+    printf("enter the number of elements in thee array\n");
+    scanf("%i",&n);
+    int a[n];
+    float avg=0;
+    printf("enter the array\n");
+    for(int i=0;i<n;i++){
+        scanf("%i",&a[i]);
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+        if(a[i]>a[j]){
+            temp = a[i];
+            a[i]= a[j];
+            a[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf(" %i",a[i]);
+    }
+}
