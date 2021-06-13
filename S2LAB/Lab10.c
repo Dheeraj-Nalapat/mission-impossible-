7f0b0f411c53545d642bdae5f3bfc5d54b09b536
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-06-14 02:49:24 +0530

    Create Lab10.c

---
+++ S2LAB/Lab10.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+main(){
+    char a[1000];
+    int i=0,j=0,v=0,total=0,t;
+    printf("enter the string ending with $\n");
+    gets(a);
+    for( t=0;a[t]!='$';t++){
+       if(a[t]=='a'){
+          v++;
+       }
+       else if(a[t]=='e'){
+          v++;
+       }
+       else if(a[t]=='i'){
+          v++;
+       }
+       else if(a[t]=='o'){
+          v++;
+       }
+       else if(a[t]=='u'){
+          v++;
+       }
+       else if(a[t]==' '){
+          j++;
+       }
+       else{
+         i++;
+       }
+      
+    }
+       
+    printf("the number of vowels is %i\n",v);
+    printf("the number of  spaces is %i\n",j);
+    printf("the number of consanants is %i\n",i);
+   
+}
