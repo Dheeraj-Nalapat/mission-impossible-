16195caefdba93d17669d8b2b745211c2f2c6807
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-06-13 23:32:48 +0530

    Create Lab6.c

---
+++ S2LAB/Lab6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+    int n,t;
+    printf("enter the number of elements in thee array\n");
+    scanf("%i",&n);
+    int a[n],sum=0;
+    float avg=0;
+    for(int i=0;i<n;i++){
+        scanf("%i",&a[i]);
+    }
+    printf("enter the number to be searched\n");
+    scanf("%i",&t);
+    for(int i=0;i<n;i++){
+        if(a[i]==t){
+            printf("element founded at index %i\n",i);
+        }
+    }
+}
