432c11c348db141175f488f536e26670b780567e
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-06-16 20:41:04 +0530

    Update Lab11.c

--- S2LAB/Lab11.c
+++ S2LAB/Lab11.c
@@ -6,13 +6,10 @@ main(){
     printf("enter the string \n");
     gets(a);
     int len=0;
-    for(len=0;a[len]!='\0';len++);
-    printf("%i",len);
-    for(int i=0;i<len;i++){
+    for(len=0;a[len]!='\0';++len);
+    for(int i=0,j=0;i<len;i++,j++){
       
-          b[len-i-1]=a[i];
-      
-       
+          b[j]=a[len-i-1]; 
     }
    printf("the string is %s\n",b);
 }
