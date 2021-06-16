7828f60650e87af615e213d90bbef675d6cb2049
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-06-16 19:41:47 +0530

    Update Lab1.c

--- S2LAB/Lab1.c
+++ S2LAB/Lab1.c
@@ -9,10 +9,10 @@ main(){
     printf("their sum is = %i\n",x+y);
     printf("enter the radius of the circle\n");
     scanf("%i",&z);
-    printf("area of the cirlce is = %i",3.14*z*z);
+    printf("area of the cirlce is = %f\n",3.14*z*z);
     float a,b,c,d,e,f,g;
     printf("enter the values of a,b,c,d,e,f,g in the order\n" );
-    scanf("%i %i %i %i %i %i %i",&a,&b,&c,&d,&e,&f,&g);
-    printf("result of ((a-b/c*d+e)*(f+g))= %i",((a-b/c*d+e)*(f+g)));
+    scanf("%f %f %f %f %f %f %f",&a,&b,&c,&d,&e,&f,&g);
+    printf("result of ((a-b/c*d+e)*(f+g))= %f\n",((a-(b/c*d+e))*(f+g)));
 
 }
