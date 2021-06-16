8c6f0ff1616beb25ca7d87d1d20b267783aab7ba
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-06-16 20:17:09 +0530

    Update Lab10.c

--- S2LAB/Lab10.c
+++ S2LAB/Lab10.c
@@ -6,6 +6,7 @@ main(){
     int i=0,j=0,v=0,total=0,t;
     printf("enter the string ending with $\n");
     gets(a);
+    toupper(a);
     for( t=0;a[t]!='$';t++){
        if(a[t]=='a'){
           v++;
@@ -20,6 +21,21 @@ main(){
           v++;
        }
        else if(a[t]=='u'){
+          v++;
+       }
+       else if(a[t]=='A'){
+          v++;
+       }
+       else if(a[t]=='E'){
+          v++;
+       }
+       else if(a[t]=='I'){
+          v++;
+       }
+       else if(a[t]=='O'){
+          v++;
+       }
+       else if(a[t]=='U'){
           v++;
        }
        else if(a[t]==' '){
