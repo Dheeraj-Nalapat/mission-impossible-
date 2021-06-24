a12b7f1bbb19098f4a89807dc800f4027751d518
Dheeraj-Nalapat <dheerajnalapat3@gmail.com>
2021-06-25 00:56:09 +0530

    commit message

---
+++ pythonprojects/pyramid message.py
@@ -0,0 +1,12 @@
+import keyboard
+import time 
+x=""
+time.sleep(3)
+
+for i in range(10):
+     x=x+"."
+     keyboard.write(x)
+     keyboard.send('Enter')
+     time.sleep(2)
+     if keyboard.is_pressed('x'):
+          break
