e1e5b89293e42256eb920661fbab981ec33c6bd3
Dheeraj-Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2021-07-24 17:54:54 +0530

    Create FCTRL.cpp

---
+++ CodeChef/FCTRL.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int findingzero(long long int n){
+    long long int res =0;
+    res = n/5;
+    if(res!=0){
+    res += findingzero(res);
+    }
+    return res;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int n;
+        cin >> n;
+        cout << findingzero(n) << endl;
+    }
+}
